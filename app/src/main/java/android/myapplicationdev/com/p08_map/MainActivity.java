5beb10c10145e8a24588058d0adcdee0d148307d
Juhan Jufri <15017395@myrp.edu.sh>
2017-07-16 19:04:13 +0800

    Initial commit

---
+++ app/src/main/java/android/myapplicationdev/com/p08_map/MainActivity.java
@@ -0,0 +1,68 @@
+package android.myapplicationdev.com.p08_map;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnNorth, btnCentral, btnEast;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                LatLng poi_Singapore = new LatLng(1.362424, 103.802342);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_Singapore,
+                        10));
+
+                UiSettings ui = map.getUiSettings();
+                ui.setCompassEnabled(true);
+                ui.setZoomControlsEnabled(true);
+                ui.setMyLocationButtonEnabled(true);
+
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                }
+            }
+        });
+
+
+
+        btnNorth = (Button) findViewById(R.id.btnNorth);
+        btnCentral = (Button) findViewById(R.id.btnCentral);
+        btnEast = (Button) findViewById(R.id.btnEast);
+
+    }
+}
